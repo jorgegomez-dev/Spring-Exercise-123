e35d83bee68766f142cbd72fd6d79b3f9eedc724
jorgegomez-dev <106641101+jorgegomez-dev@users.noreply.github.com>
2022-09-11 00:53:16 -0300

    Add files via upload

---
+++ ob-sprindatajpa/src/main/java/com/example/obsprindatajpa/ObSprindatajpaApplication.java
@@ -0,0 +1,19 @@
+package com.example.obsprindatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ObSprindatajpaApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(ObSprindatajpaApplication.class, args);
+
+		CocheRepository repository = context.getBean(CocheRepository.class);
+		Coche toyota = new Coche(null, "Toyota", "Prius", 2010);
+		repository.save(toyota);
+		System.out.println(toyota);
+	}
+
+}
